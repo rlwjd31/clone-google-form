617f14f5e453b3bed36277c1b4a4604ab833221d
rlwjd31 <rlwjd31@naver.com>
2025-02-12 15:06:43 +0900

    🔨 [Chore]: Install eslint plugin tailwindcss
    tailwind의 lint와 order를 정렬해주는 plugin 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      colors: {
+        "purple-primary": "#673AB7",
+        "purple-secondary": "#F0EBF8",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
+
+// "primary": "#3C64B1",
+// "secondary": "#F2F2F2",
+// "tertiary": "#F8F8F8",
+// "quaternary": "#E5E5E5",
+// "quinary": "#D8D8D8",
+// "senary": "#BFBFBF",
+// "septenary": "#A6A6A6",
+// "octonary": "#8C8C8C",
+// "nonary": "#737373",
+// "denary": "#595959",
+// "elevenary": "#404040",
+// "twelvary": "#262626",
+// "thirteenary": "#0D0D0D",
