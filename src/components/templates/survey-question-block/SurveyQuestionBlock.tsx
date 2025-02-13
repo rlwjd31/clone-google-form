ea21de43a3f89779008d8c98a504ecdf37579971
rlwjd31 <rlwjd31@naver.com>
2025-02-14 02:45:33 +0900

    ♻️ [Refactor]: Improve readability by splitting SurveyFooter

--- src/components/templates/survey-question-block/SurveyQuestionBlock.tsx
+++ src/components/templates/survey-question-block/SurveyQuestionBlock.tsx
@@ -11,29 +11,13 @@ import {
   setQuestionTitle,
   SurveyQuestionBlockStore,
 } from "@/components/templates/survey-question-block/slice";
+import SurveyQuestionBlockFooter from "@/components/templates/survey-question-block/SurveyQuestionBlockFooter";
 import { Provider, useDispatch, useSelector } from "react-redux";
 
-function SurveyQuestionBlockFooter() {
-  const isRequired = useSelector((state: LocalStateType) => state.isRequired);
-  const dispatch = useDispatch<LocalStateActionType>();
-  return (
-    <div className="flex justify-end py-4 mt-10 border-t border-t-neutral-200">
-      <ButtonIcon
-        iconType="content-copy"
-        visibleToolTip
-        tooltipPosition="bottom"
-      />
-      <ButtonIcon iconType="delete" visibleToolTip />
-      <div className="w-px ml-3 mr-5 bg-neutral-300" />
-      <div className="flex items-center gap-5">
-        <span>필수</span>
-        <Toggle
-          enabled={isRequired}
-          setEnabled={() => dispatch(setIsRequired(!isRequired))}
-        />
-      </div>
-    </div>
-  );
+
+
+function QuestionsByType() {
+  
 }
 
 function SurveyQuestionBlock() {
