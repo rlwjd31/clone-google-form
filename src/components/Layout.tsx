376ed1a24ce80cd8a8bec654d442b59884c40a89
rlwjd31 <rlwjd31@naver.com>
2025-02-15 08:26:21 +0900

    💄 [Design]: Enable screen scroll
    screen이 scroll되지 않아 scroll이 되게끔 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,12 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-dvh w-dvw items-center justify-center bg-purple-secondary p-12">
-      <div className="w-full max-w-[51rem]">{children}</div>
+    <div className="flex h-screen w-screen flex-col bg-purple-secondary px-16 py-12">
+      <div className="mx-auto w-full max-w-[51rem] bg-green-50 ">
+        {children}
+      </div>
     </div>
   );
 }
+
+//
