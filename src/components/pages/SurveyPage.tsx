8fb1cd9e4a12bdcd21a410d1b32de11567b20707
rlwjd31 <rlwjd31@naver.com>
2025-02-15 06:04:24 +0900

    💄 [Design]: Style big title

--- src/components/pages/SurveyPage.tsx
+++ src/components/pages/SurveyPage.tsx
@@ -1,15 +1,32 @@
+import Input from "@/components/atoms/Input";
+import Card from "@/components/molecules/Card";
 import SurveyQuestionBlock from "@/components/templates/survey-question-block/SurveyQuestionBlock";
 import { SurveyIdProvider } from "@/store/SurveyIdProvider";
 import { GlobalState } from "@/store/surveys.slice";
+import { cn } from "@/utils/cn";
 import { useSelector } from "react-redux";
 
 export default function SurveyPage() {
   const surveys = useSelector((state: GlobalState) => state.surveysState);
 
   return (
-    <div className="flex size-full flex-col items-center gap-8">
-      {/* TODO: survy big Title */}
-      <div>survey big title</div>
+    <div className="flex flex-col items-center gap-8 size-full">
+      <Card
+        className="relative overflow-hidden pb-6"
+        <div className="absolute left-0 top-0 z-20 h-3 w-full bg-purple-primary" />
+        <div className="flex w-full flex-col gap-1">
+          <Input.Title
+            className={cn(
+              "mt-6",
+            )}
+          />
+          <Input.Description
+            className={cn(
+            )}
+            placeholder="설문지 설명"
+          />
+        </div>
+      </Card>
       {surveys.map(({ surveyId }) => (
         <SurveyIdProvider surveyIdProp={surveyId}>
           <SurveyQuestionBlock />
