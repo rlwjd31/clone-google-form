1da536e11ca9c626c2d951421c37eb7f7ee82232
rlwjd31 <rlwjd31@naver.com>
2025-02-15 06:16:15 +0900

    💄 [Design]: Restyle root layout maw width
    48rem -> 51rem으로 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-dvh w-dvw items-center justify-center bg-purple-secondary p-12">
-      {children}
+      <div className="w-full max-w-[51rem]">{children}</div>
     </div>
   );
 }
