bf1030f33d2a46919d0a7b05a6ece2f3101120b7
rlwjd31 <rlwjd31@naver.com>
2025-02-15 14:06:52 +0900

    🚀 [Feat]: Add PreviewRadioGroup organism component
    미리보기모드 시 객관식 질문 종류 일 때 rendering할 컴포넌트 구현

---
+++ src/components/organisms/PreviewRadioGroup.tsx
@@ -0,0 +1,30 @@
+import RadioOption from "@/components/molecules/RadioOption";
+import { OptionType } from "@/types/option.type";
+import { cn } from "@/utils/cn";
+import { ComponentProps, useState } from "react";
+
+type RadioGroupProps = ComponentProps<"div"> & {
+  options: Array<OptionType>;
+  className?: string;
+};
+
+export default function RadioGroup({ className, options }: RadioGroupProps) {
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    undefined
+  );
+
+  return (
+    <div className={cn("flex flex-col gap-1", className)}>
+      {options.map(({ optionId, value }, idx) => (
+        <RadioOption
+          key={optionId}
+          isActivated={value === selectedValue}
+          onClickHandler={() =>
+            setSelectedValue((prev) => (prev !== value ? value : ""))
+          }
+          value={value ?? `옵션 ${idx + 1}`}
+        />
+      ))}
+    </div>
+  );
+}
