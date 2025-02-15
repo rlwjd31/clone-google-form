f3cdbd5af1cb0060595b88e300a37373d02125d4
rlwjd31 <rlwjd31@naver.com>
2025-02-15 14:07:14 +0900

    🚀 [Feat]: Add PreviewRadioGroup organism component
    미리보기모드 시 객관식 질문 종류 일 때 rendering할 컴포넌트 구현

---
+++ src/components/organisms/PreviewCheckboxGroup.tsx
@@ -0,0 +1,37 @@
+import CheckboxOption from "@/components/molecules/CheckboxOption";
+import { OptionType } from "@/types/option.type";
+import { cn } from "@/utils/cn";
+import { ComponentProps, useState } from "react";
+
+type RadioGroupProps = ComponentProps<"div"> & {
+  options: Array<OptionType>;
+  className?: string;
+};
+
+export default function PreviewCheckboxGroup({
+  className,
+  options,
+}: RadioGroupProps) {
+  const [selectedValue, setSelectedValue] = useState<string[]>([]);
+
+  const toggleSelectedValue = (option: string) => {
+    setSelectedValue((prev) =>
+      prev.includes(option)
+        ? prev.filter((v) => v !== option)
+        : [...prev, option]
+    );
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-1", className)}>
+      {options.map(({ optionId, value }, idx) => (
+        <CheckboxOption
+          key={optionId}
+          isActivated={selectedValue.includes(value)}
+          onClickHandler={() => toggleSelectedValue(value)}
+          value={value ?? `옵션 ${idx + 1}`}
+        />
+      ))}
+    </div>
+  );
+}
