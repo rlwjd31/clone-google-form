a9a00d39e14b1ec6487ee9758145fe8ec56f49f7
rlwjd31 <rlwjd31@naver.com>
2025-02-15 15:39:36 +0900

    🚀 [Feat]: Render Preview buttionicon
    미리보기 기능을 제공할 button icon rendering로직 구현
    또한 router의 구조로 인해 Layout 컴포넌트를 해당 page내부로 옮김

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,16 @@
+import ButtonIcon from "@/components/molecules/ButtonIcon";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col w-screen h-screen py-12 pl-8 pr-24 bg-purple-secondary">
+    <div className="realative flex h-screen w-screen flex-col bg-purple-secondary pb-12 pl-8 pr-24 pt-20">
+      <div className="fixed left-0 top-0 z-50 flex h-12 w-screen justify-center bg-card">
+        <ButtonIcon
+          iconType="preview"
+          visibleToolTip
+          onClick={() => alert("미리보기 모드로 전환합니다.")}
+        />
+      </div>
       <div className="mx-auto w-full max-w-[51rem]">{children}</div>
     </div>
   );
--- src/components/pages/PreviewPage.tsx
+++ src/components/pages/PreviewPage.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/atoms/Input";
+import Layout from "@/components/Layout";
 import Card from "@/components/molecules/Card";
 import PreviewQuestionsBlock from "@/components/templates/PreviewQuestionsBlock";
 import { SurveyIdProvider } from "@/store/SurveyIdProvider";
@@ -16,38 +17,40 @@ export default function PreviewPage() {
   }));
 
   return (
-    <div className="relative flex w-full flex-col items-center gap-4 pb-10 [&_.hidden-preview-mode]:pointer-events-none">
-      <Card className="relative w-full pb-6" draggable={false}>
-        <div className="absolute left-0 top-0 z-20 h-3 w-full rounded-t-md bg-purple-primary" />
-        <div className="flex w-full flex-col gap-1">
-          <Input.Title
-            className={"pointer-events-none mt-6"}
-            placeholder="설문지 제목"
-            value={surveyTitle}
-          />
-          <Input.Description
-            className="pointer-events-none"
-            placeholder="설문지 설명"
-            value={description}
-          />
-        </div>
-      </Card>
-
-      {/* Survey rendering영역 */}
-      <div className="relative flex w-full flex-col gap-4">
-        {surveys.map(({ surveyId }, index) => (
-          <div
-            key={surveyId}
-            ref={(element) => {
-              surveyRefs.current[index] = element!;
-            }}
-          >
-            <SurveyIdProvider surveyIdProp={surveyId}>
-              <PreviewQuestionsBlock />
-            </SurveyIdProvider>
+    <Layout>
+      <div className="relative flex w-full flex-col items-center gap-4 pb-10 [&_.hidden-preview-mode]:pointer-events-none">
+        <Card className="relative w-full pb-6" draggable={false}>
+          <div className="absolute left-0 top-0 z-20 h-3 w-full rounded-t-md bg-purple-primary" />
+          <div className="flex w-full flex-col gap-1">
+            <Input.Title
+              className={"pointer-events-none mt-6"}
+              placeholder="설문지 제목"
+              value={surveyTitle}
+            />
+            <Input.Description
+              className="pointer-events-none"
+              placeholder="설문지 설명"
+              value={description}
+            />
           </div>
-        ))}
+        </Card>
+
+        {/* Survey rendering영역 */}
+        <div className="relative flex w-full flex-col gap-4">
+          {surveys.map(({ surveyId }, index) => (
+            <div
+              key={surveyId}
+              ref={(element) => {
+                surveyRefs.current[index] = element!;
+              }}
+            >
+              <SurveyIdProvider surveyIdProp={surveyId}>
+                <PreviewQuestionsBlock />
+              </SurveyIdProvider>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
