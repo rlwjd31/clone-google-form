02f0232d09d168356bb9b1e0de032a54176498d8
rlwjd31 <rlwjd31@naver.com>
2025-02-16 11:33:50 +0900

    🚀 [Feat]: Connect react hook form with page
    page와 react-hook-form연결
    
    - 각 질문 옵션들에 대한 input은 추후 hidden으로 값을 검증하기 위함
    - surveys에 따라 formName와 input이 초기화

--- src/components/pages/PreviewPage.tsx
+++ src/components/pages/PreviewPage.tsx
@@ -8,7 +8,9 @@ import Card from "@/components/molecules/Card";
 import PreviewQuestionsBlock from "@/components/templates/PreviewQuestionsBlock";
 import { SurveyIdProvider } from "@/store/SurveyIdProvider";
 import { GlobalState } from "@/store/surveys.slice";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
+import { CustomFormProvider } from "@/store/CustomFormProvider";
+import { createFormName } from "@/utils/createFormName";
 
 export default function PreviewPage() {
   const surveys = useSelector((state: GlobalState) => state.surveysState);
@@ -17,53 +19,81 @@ export default function PreviewPage() {
     surveyTitle: state.surveyTitle,
     description: state.description,
   }));
-  const { register, control, handleSubmit } = useForm({ mode: "all" });
-  const isRequiredExisted = useSelector((state: GlobalState) =>
-    state.surveysState.some((survey) => survey.state.isRequired)
-  );
+  const formMethods = useForm({
+    mode: "all",
+  });
+
+  const { register, control, handleSubmit } = formMethods;
+
+  const isRequiredExisted = surveys.some((survey) => survey.state.isRequired);
 
   return (
-    <Layout>
-      <div className="relative flex w-full flex-col items-center gap-4 pb-10 [&_.hidden-preview-mode]:pointer-events-none">
-        <Card className="relative w-full pb-6" draggable={false}>
-          <div className="absolute left-0 top-0 z-20 h-3 w-full rounded-t-md bg-purple-primary" />
-          <div className="flex w-full flex-col gap-1">
-            <Input.Title
-              className={"pointer-events-none mt-6"}
-              placeholder="설문지 제목"
-              value={surveyTitle}
-            />
-            <Input.Description
-              className="pointer-events-none"
-              placeholder="설문지 설명"
-              value={description}
-            />
-          </div>
-          {isRequiredExisted && (
-            <div className="mt-6 border-t border-neutral-300 pt-4 text-red-primary">
-              * 표시는 필수 질문임
-            </div>
+    <FormProvider {...formMethods}>
+      <Layout>
+        <form
+          className=""
+          onSubmit={handleSubmit((formData) =>
+            console.log("submit event formData =>", formData)
           )}
-        </Card>
+        >
+          {/* 설문지 제목 영역 */}
+          <section className="relative flex w-full flex-col items-center gap-4 pb-10 [&_.hidden-preview-mode]:pointer-events-none">
+            <Card className="relative w-full pb-6" draggable={false}>
+              <div className="absolute left-0 top-0 z-20 h-3 w-full rounded-t-md bg-purple-primary" />
+              <div className="flex w-full flex-col gap-1">
+                <Input.Title
+                  className={"pointer-events-none mt-6"}
+                  placeholder="설문지 제목"
+                  value={surveyTitle}
+                />
+                <Input.Description
+                  className="pointer-events-none"
+                  placeholder="설문지 설명"
+                  value={description}
+                />
+              </div>
+              {isRequiredExisted && (
+                <div className="mt-6 border-t border-neutral-300 pt-4 text-red-primary">
+                  * 표시는 필수 질문임
+                </div>
+              )}
+            </Card>
 
-        {/* Survey rendering영역 */}
-        <>
-          {surveys.map(({ surveyId }, index) => (
-            <div
-              key={surveyId}
-              ref={(element) => {
-                surveyRefs.current[index] = element!;
-              }}
-            >
-              <SurveyIdProvider surveyIdProp={surveyId}>
-                <PreviewQuestionsBlock />
-              </SurveyIdProvider>
-            </div>
-          ))}
-        </div>
+            {/* Survey rendering section */}
+            <section className="relative flex w-full flex-col gap-4">
+              {surveys.map(
+                ({ surveyId, state: { isRequired, questionType } }, index) => {
+                  const formName = createFormName({ surveyId, questionType });
+
+                  return (
+                    <>
+                      <div
+                        key={surveyId}
+                        ref={(element) => {
+                          surveyRefs.current[index] = element!;
+                        }}
+                      >
+                        <SurveyIdProvider surveyIdProp={surveyId}>
+                          <CustomFormProvider formName={formName}>
+                            <PreviewQuestionsBlock />
+                          </CustomFormProvider>
+                        </SurveyIdProvider>
+                      </div>
+                      <input
+                        placeholder="validation hidden input"
+                        {...register(formName, {
+                          required: isRequired,
+                        })}
+                      />
+                    </>
+                  );
+                }
+              )}
+            </section>
+          </section>
           <DevTool control={control} />
-        </>
-      </div>
-    </Layout>
+        </form>
+      </Layout>
+    </FormProvider>
   );
 }
