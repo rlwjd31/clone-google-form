89b1cc8fe3001dc71dbfa61c88b8350645cc628a
rlwjd31 <rlwjd31@naver.com>
2025-02-16 11:31:33 +0900

    🚀 [Feat]: Create context to pass formname without props drilling
    각 form에 검증되는 input에 전달되는 name은 동적으로 할당되며 atom단위까지 전달되어야하는 이슈가 있어
    context api를 이용하여 formname을 내려주어 나중에 state와 input value를 연동하기 위함

---
+++ src/store/CustomFormProvider.tsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, ReactNode } from "react";
+
+type CustomFormProviderProps = { formName: string };
+
+const CustomFormContext = createContext<CustomFormProviderProps | null>(null);
+
+export const CustomFormProvider = ({
+  children,
+  formName,
+}: {
+  children: ReactNode;
+  formName: string;
+}) => {
+  return (
+    <CustomFormContext.Provider value={{ formName }}>
+      {children}
+    </CustomFormContext.Provider>
+  );
+};
+
+export const useCustomFormProvider = () => {
+  const context = useContext(CustomFormContext);
+  if (!context)
+    throw new Error(
+      "useCustomFormProvider must be used within CustomFormProvider"
+    );
+  return context;
+};
